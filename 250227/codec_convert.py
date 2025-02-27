1117dc33eee80320d2f489059734e35d7eac6573
souok92 <89488287+souok92@users.noreply.github.com>
2025-02-27 16:07:48 +0900

    new
    new

---
+++ 250227/codec_convert.py
@@ -0,0 +1,37 @@
+import ffmpeg
+import os
+
+def convert_ms_cram_to_mp4(input_file, output_file):
+    if not os.path.exists(input_file):
+        raise FileNotFoundError(f"Input file '{input_file}' not found.")
+    
+    try:
+        (
+            ffmpeg
+            .input(input_file)
+            .output(output_file, vcodec='libx264', pix_fmt='yuv420p')
+            .run(overwrite_output=True)
+        )
+        print(f"Conversion successful: {output_file}")
+    except ffmpeg.Error as e:
+        print("Error occurred:", e)
+        print(e.stderr.decode())
+
+def convert_folder_avi_to_mp4(folder_path, output_folder):
+    if not os.path.exists(folder_path):
+        raise FileNotFoundError(f"Folder '{folder_path}' not found.")
+    
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+    
+    for file in os.listdir(folder_path):
+        if file.lower().endswith(".avi"):
+            input_file = os.path.join(folder_path, file)
+            output_file = os.path.join(output_folder, os.path.splitext(file)[0] + ".mp4")
+            print(f"Converting {input_file} to {output_file}...")
+            convert_ms_cram_to_mp4(input_file, output_file)
+
+if __name__ == "__main__":
+    folder_path = "./"  # 현재 폴더 내의 모든 AVI 파일 변환
+    output_folder = "./converted_videos"  # 변환된 파일 저장 폴더
+    convert_folder_avi_to_mp4(folder_path, output_folder)
---
+++ 250227/crop_image.py
@@ -0,0 +1,23 @@
+import cv2
+
+# 이미지 파일 경로
+image_path = "./TEST_0002.BMP"
+
+# 이미지 로드
+image = cv2.imread(image_path)
+
+# 자를 좌표 설정
+x_start, x_end = 311, 765
+y_start, y_end = 101, 579
+
+# 이미지 자르기
+cropped_image = image[y_start:y_end, x_start:x_end]
+
+# 저장할 파일 경로
+cropped_image_path = "./cropped_image.BMP"
+
+# 자른 이미지 저장
+cv2.imwrite(cropped_image_path, cropped_image)
+
+# 결과 출력
+print(f"가운데 부분을 자른 이미지가 저장되었습니다: {cropped_image_path}")
